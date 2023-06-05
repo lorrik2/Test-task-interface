fd4d125203a9de5ec5103ff7e8a5a9b4742bb644
Matvey <lorrik1703@yandex.ru>
2023-06-05 14:59:36 +0300

    navbar compo + styles

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.scss';
+import Navbar from '../features/menu/navbar/Navbar';
 
 function App(): JSX.Element {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/features/menu/navbar/Navbar.tsx
+++ frontend/src/features/menu/navbar/Navbar.tsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import './scss/navbar.scss';
+import { NavLink } from 'react-router-dom';
+
+import burgerClose from './assets/burger-close-menu.svg';
+import burgerMenu from './assets/burger-menu.svg';
+import logo from './assets/ic_logo.svg';
+import person from './assets/ic_person.svg';
 
 function Navbar(): JSX.Element {
-  return <div>Navbar</div>;
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  const showNavbar = (): void => {
+    if (navRef && navRef.current) {
+      navRef.current.classList.toggle('responsive__nav');
+    }
+  };
+
+  return (
+    <header>
+      <div className="flex__container">
+        <img src={logo} alt="logo" />
+        <h3>Wrench CRM</h3>
+      </div>
+
+      <nav ref={navRef}>
+        <NavLink to="/">Главная</NavLink>
+        <NavLink to="/address">Поиск адресов</NavLink>
+        <NavLink to="/#">Таблицы</NavLink>
+        <NavLink to="/#">Календарь</NavLink>
+        <NavLink to="/#">Карты</NavLink>
+        <NavLink to="/#">Виджеты</NavLink>
+        <NavLink to="/#">Настройки</NavLink>
+        {/*добавить*/}
+        <NavLink to="/#">Выход</NavLink>
+        <button className="nav__btn nav-close-btn" onClick={showNavbar}>
+          <img src={burgerClose} alt="close" />
+        </button>
+      </nav>
+      <div className="flex__container">
+        <img src={person} alt="person" />
+        <h4 className="nav__user-name">Имя Фамилия</h4>
+      </div>
+      <button className="nav__btn" onClick={showNavbar}>
+        <img src={burgerMenu} alt="open" />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
